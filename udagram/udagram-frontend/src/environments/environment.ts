8cc62035e4ec112a050f30368dcafb006cad52db
HendIbrahim2000 <hendibrahim2000@gmail.com>
2023-03-08 22:14:03 +0200

    modify

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-b8msbwdf.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost:'http://udagram-api3-dev.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
