56843f597114b90c06300706e857734ce17d0027
HendIbrahim2000 <hendibrahim2000@gmail.com>
2023-03-08 06:14:47 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=password123
+export POSTGRES_HOST=database-1.ce171edbghly.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgress
+export AWS_BUCKET=arn:aws:s3:::my-653597783644-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=anyJWT
+export URL=http://localhost:8100
