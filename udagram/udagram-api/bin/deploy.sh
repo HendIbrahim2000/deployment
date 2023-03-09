6f508186e7115bbf702c1f787251b681d8b58f46
HendIbrahim2000 <hendibrahim2000@gmail.com>
2023-03-09 16:41:26 +0200

    modify

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,15 +1,21 @@
 cd www
-eb list
-eb use udagram-api3-dev
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-eb setenv AWS_PROFILE=$AWS_PROFILE
-eb setenv AWS_BUCKET=$AWS_BUCKET
-eb setenv URL=$URL
-eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+if [[ ! -v POSTGRES_HOST ]]; then
+        echo "POSTGRES_HOST is not set and so the others"
+        eb list
+        eb use udagram-api3-dev
+        echo "setting env variables in eb env"
+        eb setenv POSTGRES_HOST=$POSTGRES_HOST
+        eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+        eb setenv POSTGRES_DB=$POSTGRES_DB
+        eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+        eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+        eb setenv AWS_PROFILE=$AWS_PROFILE
+        eb setenv AWS_BUCKET=$AWS_BUCKET
+        eb setenv URL=$URL
+        eb setenv JWT_SECRET=$JWT_SECRET
+        eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+        eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+else
+    echo "POSTGRES_HOST has been set so the others"
+fi
 eb deploy udagram-api3-dev --profile default
